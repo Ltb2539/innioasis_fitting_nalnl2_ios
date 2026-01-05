b2197461445293db4a1bf93d2d515f8e99f1206c
innioasis_mac <253616768@qq.com>
2026-01-05 10:57:43 +0800

    no message

---
+++ DCTestUniPlugin/DCTestUniPluginTests/DCTestUniPluginTests.m
@@ -0,0 +1,37 @@
+//
+//  DCTestUniPluginTests.m
+//  DCTestUniPluginTests
+//
+//  Created by innioasis on 2026/1/5.
+//  Copyright © 2026 DCloud. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DCTestUniPluginTests : XCTestCase
+
+@end
+
+@implementation DCTestUniPluginTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
